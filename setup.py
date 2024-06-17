298c67096f73ed3a801eaeb08adef85fa84ba68a
Rizal Fadlullah <izzal@MacBook-Air-Rizal.local>
2024-06-17 12:42:28 +0700

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='izlearn',
     packages=find_packages(exclude=['test']),
-    version='0.1.0',
+    version='0.1.1',
     description='Test buat library python',
     author='izzalDev',
     license='MIT',
