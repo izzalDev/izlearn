a3a666c2cac9ab4967fb0882b71a972f140c7381
Rizal Fadlullah <izzal@MacBook-Air-Rizal.local>
2024-06-19 00:04:16 +0700

    First Release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='izlearn',
     packages=find_packages(exclude=['test']),
-    version='0.1.0',
+    version='1.0.0',
     description='Python package for simplifying machine learning projects',
     author='izzalDev',
     license='MIT',
