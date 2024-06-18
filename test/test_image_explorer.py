359f2b46c1d5898417c184be4fe902ed070f2705
Rizal Fadlullah <izzal@MacBook-Air-Rizal.local>
2024-06-18 23:07:28 +0700

    Checking for first release

--- test/test_image_explorer.py
+++ test/test_image_explorer.py
@@ -2,3 +2,14 @@
 
 import unittest
 from izlearn.vision import ImageExplorer
+
+class TestImportImageExplorer(unittest.TestCase):
+
+    def test_import(self):
+        try:
+            from izlearn.vision import ImageExplorer
+        except ImportError:
+            self.fail("ImportError: cannot import 'ImageExplorer' from 'izlearn.vision'")
+
+if __name__ == '__main__':
+    unittest.main()
